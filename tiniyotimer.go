a56eb78637d33f02a074651dcc906f1169132c5d
sptiniyo <sptiniyo@tiniyo.com>
2020-02-19 19:09:03 +0530

    sync map

--- tiniyotimer.go
+++ tiniyotimer.go
@@ -1,11 +1,13 @@
 package timer
 
 import (
+	"sync"
+
 	timer "github.com/singchia/go-timer"
 )
 
 type TiniyoTimer struct {
-	m       map[string]timer.Tick
+	m       sync.Map
 	ch      chan interface{}
 	handler TiniyoTimerHandler
 	t       timer.Timer
@@ -21,23 +23,22 @@ type TiniyoTimerHandler func(data interface{}) error
 /* message id to timer tick */
 
 func (tt *TiniyoTimer) InitializeTiniyoTimer(cbHandler TiniyoTimerHandler) {
-	tt.m = make(map[string]timer.Tick)
 	tt.ch = make(chan interface{})
 	tt.handler = cbHandler
 	tt.t = timer.NewTimer()
 	tt.t.Start()
 }
 
-func (this *TiniyoTimer) Run() {
+func (tt *TiniyoTimer) Run() {
 	go func() {
 		for {
-			data, ok := <-this.ch
+			data, ok := <-tt.ch
 			if ok == false {
 				break
 			} else {
-				this.handler(data.(timerdata).data)
+				tt.handler(data.(timerdata).data)
 				/* remove from map */
-				delete(this.m, data.(timerdata).timer_id)
+				tt.m.Delete(data.(timerdata).timer_id)
 			}
 		}
 	}()
@@ -49,12 +50,13 @@ func (tt *TiniyoTimer) StartTimer(t uint64, timerid string, data interface{}) er
 	if err != nil {
 		return err
 	}
-	tt.m[timerid] = t1
+	tt.m.Store(timerid, t1)
 	return nil
 }
 
-func (tt *TiniyoTimer) CancelTimer(msgid string) {
-	tt.m[msgid].Cancel()
+func (tt *TiniyoTimer) CancelTimer(timerid string) {
+	timerobj, _ := tt.m.Load(timerid)
+	timerobj.(timer.Tick).Cancel()
 	/* remove from map */
-	delete(tt.m, msgid)
+	tt.m.Delete(timerid)
 }
